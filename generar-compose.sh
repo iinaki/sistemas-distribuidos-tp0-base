be5aa6cce084b1104f97c92e33b23c087344b7ce
iinaki <llorensinaki22@gmail.com>
2025-08-24 18:37:54 -0300

    Add bash script, think everything is working

---
+++ generar-compose.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# Como respuesta del ejercicio 1 creamos un script de bash para generar un docker compose con N clientes
+
+if [ "$#" -ne 2 ]; then
+    echo "Uso: $0 <archivo-salida.yaml> <cantidad-clientes>"
+    exit 1
+fi
+
+OUTPUT_FILE="$1"
+CLIENT_COUNT="$2"
+
+echo "Nombre del archivo de salida: $OUTPUT_FILE"
+echo "Cantidad de clientes: $CLIENT_COUNT"
+
+cat > "$OUTPUT_FILE" <<EOF
+name: tp0
+services:
+  server:
+    container_name: server
+    image: server:latest
+    entrypoint: python3 /main.py
+    environment:
+      - PYTHONUNBUFFERED=1
+      - LOGGING_LEVEL=DEBUG
+    networks:
+      - testing_net
+EOF
+
+for i in $(seq 1 "$CLIENT_COUNT"); do
+    cat >> "$OUTPUT_FILE" <<EOF
+  client${i}:
+    container_name: client${i}
+    image: client:latest
+    entrypoint: /client
+    environment:
+      - CLI_ID=${i}
+      - CLI_LOG_LEVEL=DEBUG
+    networks:
+      - testing_net
+    depends_on:
+      - server
+EOF
+done
+
+cat >> "$OUTPUT_FILE" <<EOF
+networks:
+  testing_net:
+    ipam:
+      driver: default
+      config:
+        - subnet: 172.25.125.0/24
+EOF
+
+echo "Archivo '$OUTPUT_FILE' generado con $CLIENT_COUNT clientes."
