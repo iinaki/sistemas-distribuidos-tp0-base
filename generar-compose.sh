f32a7c5dcfdff00f7883710d3356ded0ffe4a24b
iinaki <llorensinaki22@gmail.com>
2025-08-26 17:38:50 -0300

    Remover declaracion innecesaria de log

--- generar-compose.sh
+++ generar-compose.sh
@@ -20,7 +20,6 @@ services:
     entrypoint: python3 /main.py
     environment:
       - PYTHONUNBUFFERED=1
-      - LOGGING_LEVEL=DEBUG
     networks:
       - testing_net
     volumes:
@@ -35,7 +34,6 @@ for i in $(seq 1 "$CLIENT_COUNT"); do
     entrypoint: /client
     environment:
       - CLI_ID=${i}
-      - CLI_LOG_LEVEL=DEBUG
     networks:
       - testing_net
     depends_on:
