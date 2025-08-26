aa9cae394bfabe9b323c3c102cea1ced878b7b0b
iinaki <llorensinaki22@gmail.com>
2025-08-26 19:07:01 -0300

    Eliminar recibo de mensaje por parametro, creo que con un mensaje hardcodeado basta

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-# Como usar: ./validar-echo-server.sh [mensaje]
-
 set -euo pipefail
 
-MSG=$1
+MSG="testing echo server"
 SERVER_NAME="server"
 NETWORK_NAME="tp0_testing_net"
 PORT="12345"
@@ -14,24 +12,6 @@ if ! docker ps --format '{{.Names}}' | grep -qx "${SERVER_NAME}"; then
   echo "action: test_echo_server | result: fail"
   exit 1
 fi
-
-# Esperar a que el server acepte conexiones (con un retry de 10)
-# ATTEMPTS=10
-# SLEEP_SECS=1
-# READY=0
-# for _ in $(seq 1 "${ATTEMPTS}"); do
-#   if docker run --rm --network "${NETWORK_NAME}" busybox sh -c "nc -w 1 -z ${SERVER_NAME} ${PORT}" >/dev/null 2>&1; then
-#     READY=1
-#     break
-#   fi
-#   sleep "${SLEEP_SECS}"
-# done
-
-# if [[ "${READY}" -ne 1 ]]; then
-#   echo "SERVER NO ACEPTONUNCA LAS CONEXIONES"
-#   echo "action: test_echo_server | result: fail"
-#   exit 1
-# fi
 
 RESPONSE="$(docker run --rm --network "${NETWORK_NAME}" busybox sh -c "echo -n '${MSG}' | nc -w 3 ${SERVER_NAME} ${PORT}" || true)"
 
