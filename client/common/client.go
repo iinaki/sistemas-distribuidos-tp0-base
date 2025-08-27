7d5c1298ca57391680b1934bb8517f0558160dca
iinaki <llorensinaki22@gmail.com>
2025-08-27 17:43:51 -0300

    Agrego manejo de señales en el cliente

--- client/common/client.go
+++ client/common/client.go
@@ -21,15 +21,17 @@ type ClientConfig struct {
 
 // Client Entity that encapsulates how
 type Client struct {
-	config ClientConfig
-	conn   net.Conn
+	config  ClientConfig
+	conn    net.Conn
+	running bool
 }
 
 // NewClient Initializes a new client receiving the configuration
 // as a parameter
 func NewClient(config ClientConfig) *Client {
 	client := &Client{
-		config: config,
+		config:  config,
+		running: true,
 	}
 	return client
 }
@@ -54,9 +56,16 @@ func (c *Client) createClientSocket() error {
 func (c *Client) StartClientLoop() {
 	// There is an autoincremental msgID to identify every message sent
 	// Messages if the message amount threshold has not been surpassed
-	for msgID := 1; msgID <= c.config.LoopAmount; msgID++ {
+	for msgID := 1; msgID <= c.config.LoopAmount && c.running; msgID++ {
 		// Create the connection the server in every loop iteration. Send an
-		c.createClientSocket()
+		err := c.createClientSocket()
+		if err != nil {
+			log.Errorf("action: create_socket | result: fail | client_id: %v | error: %v",
+				c.config.ID,
+				err,
+			)
+			return
+		}
 
 		// TODO: Modify the send to avoid short-write
 		fmt.Fprintf(
@@ -66,7 +75,7 @@ func (c *Client) StartClientLoop() {
 			msgID,
 		)
 		msg, err := bufio.NewReader(c.conn).ReadString('\n')
-		c.conn.Close()
+		c.closeConnection()
 
 		if err != nil {
 			log.Errorf("action: receive_message | result: fail | client_id: %v | error: %v",
@@ -86,4 +95,26 @@ func (c *Client) StartClientLoop() {
 
 	}
 	log.Infof("action: loop_finished | result: success | client_id: %v", c.config.ID)
+}
+
+func (c *Client) closeConnection() {
+	if c.conn != nil {
+		err := c.conn.Close()
+		if err != nil {
+			log.Errorf("action: close_connection | result: fail | client_id: %v | error: %v",
+				c.config.ID,
+				err,
+			)
+		} else {
+			log.Debugf("action: close_connection | result: success | client_id: %v", c.config.ID)
+		}
+		c.conn = nil
+	}
+}
+
+func (c *Client) Stop() {
+	log.Infof("action: shutdown_client | result: in_progress | client_id: %v", c.config.ID)
+	c.running = false
+	c.closeConnection()
+	log.Infof("action: shutdown_client | result: success | client_id: %v", c.config.ID)
 }
