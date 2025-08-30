2f50ee3e17429a9ae540c7bc99278821e719e736
iinaki <llorensinaki22@gmail.com>
2025-08-30 10:10:56 -0300

    Agrgo send en protocolo del server

--- server/common/protocol.py
+++ server/common/protocol.py
@@ -7,6 +7,25 @@ from typing import Dict, Any, Optional
 
 class Protocol:
     @staticmethod
+    def send_message(sock: socket.socket, message: Dict[str, Any]) -> None:
+        try:
+            json_data = json.dumps(message).encode("utf-8")
+
+            header = struct.pack("!I", len(json_data))
+
+            full_message = header + json_data
+
+            Protocol._send_exact(sock, full_message)
+
+            logging.debug(
+                f"action: send_message | result: success | bytes_sent: {bytes_sent}"
+            )
+
+        except Exception as e:
+            logging.error(f"action: send_message | result: fail | error: {e}")
+            raise
+
+    @staticmethod
     def receive_message(sock: socket.socket) -> Optional[Dict[str, Any]]:
         try:
             header_data = Protocol._receive_exact(sock, 4)
@@ -39,3 +58,12 @@ class Protocol:
                 return None
             data += chunk
         return data
+
+    @staticmethod
+    def _send_exact(sock: socket.socket, full_message: bytes) -> None:
+        bytes_sent = 0
+        while bytes_sent < len(full_message):
+            sent = sock.send(full_message[bytes_sent:])
+            if sent == 0:
+                raise ConnectionError("Socket connection broken")
+            bytes_sent += sent
