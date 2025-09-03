7c73c984eb6bb3ac1bd8bc13cd58fe9f6a51afd2
iinaki <llorensinaki22@gmail.com>
2025-09-02 21:43:23 -0300

    Crear un solo client socket en el cliente

--- client/common/client.go
+++ client/common/client.go
@@ -133,6 +133,13 @@ func (c *Client) StartClientLoop() {
 
 	log.Infof("action: load_bets | result: success | client_id: %v | total_bets: %d", c.config.ID, len(bets))
 
+	err = c.createClientSocket()
+	if err != nil {
+		log.Errorf("action: create_persistent_socket | result: fail | client_id: %v | error: %v", c.config.ID, err)
+		return
+	}
+	defer c.closeConnection()
+
 	protocol := NewProtocol()
 
 	err = c.sendBets(protocol, bets)
@@ -179,13 +186,6 @@ func (c *Client) Stop() {
 }
 
 func (c *Client) sendBets(protocol *Protocol, bets []Bet) error {
-	err := c.createClientSocket()
-	if err != nil {
-		log.Errorf("action: create_persistent_socket | result: fail | client_id: %v | error: %v", c.config.ID, err)
-		return err
-	}
-	defer c.closeConnection()
-
 	batchCount := 0
 
 	for i := 0; i < len(bets) && c.running; i += c.config.BatchMaxAmount {
@@ -233,33 +233,18 @@ func (c *Client) sendBets(protocol *Protocol, bets []Bet) error {
 			log.Errorf("action: apuesta_enviada | result: fail | client_id: %v | batch_id: %d | cantidad: %d",
 				c.config.ID, batchCount, len(batch))
 		}
-
-		// Wait a time between sending one message and the next one
-		// if end < len(bets) && c.running {
-		// 	time.Sleep(c.config.LoopPeriod)
-		// }
 	}
 
 	log.Infof("action: loop_finished | result: success | client_id: %v | total_batches: %d", c.config.ID, batchCount)
-
-	c.closeConnection()
-	time.Sleep(c.config.LoopPeriod)
 
 	return nil
 }
 
 func (c *Client) sendFinishedSending(protocol *Protocol) error {
-	err := c.createClientSocket()
-	if err != nil {
-		log.Errorf("action: create_persistent_socket | result: fail | client_id: %v | error: %v", c.config.ID, err)
-		return err
-	}
-	defer c.closeConnection()
-
 	message := &FinishedSendingMessage{AgencyID: c.config.ID}
 	messageBytes := message.ToBytes()
 
-	err = protocol.SendMessage(c.conn, MsgTypeFinishedSending, messageBytes)
+	err := protocol.SendMessage(c.conn, MsgTypeFinishedSending, messageBytes)
 	if err != nil {
 		return fmt.Errorf("error sending finished sending: %v", err)
 	}
@@ -288,13 +273,6 @@ func (c *Client) sendFinishedSending(protocol *Protocol) error {
 
 func (c *Client) requestWinners(protocol *Protocol) error {
 	for {
-		if c.conn == nil {
-			err := c.createClientSocket()
-			if err != nil {
-				return fmt.Errorf("error creating socket for winners request: %v", err)
-			}
-		}
-
 		message := &WinnersRequestMessage{AgencyID: c.config.ID}
 		messageBytes := message.ToBytes()
 
@@ -310,7 +288,6 @@ func (c *Client) requestWinners(protocol *Protocol) error {
 
 		if messageType == MsgTypeLotteryNotReady {
 			log.Infof("action: lottery_not_ready | result: in_progress | client_id: %s", c.config.ID)
-			c.closeConnection()
 			time.Sleep(100 * time.Millisecond)
 			continue
 		}
